934a8fbd024602c6ad587d56e83feb66260cab51
Nelya-Maleeva <nelyamaleeva@gmail.com>
2023-03-28 12:23:19 +0300

    qweqwe

--- 2803/2803/Program.cs
+++ 2803/2803/Program.cs
@@ -7,6 +7,8 @@ namespace _2803
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Nelya 2803!");
+            Console.WriteLine("Hello Nelya 2803333333333333333333333333333333333333333333333333333333333333333333333333333333333333333!");
+            Console.WriteLine("Hell222222222222222222o Ne22222222222222222222222lya 22222222222222222222222222222!");
         }
     }
 }
