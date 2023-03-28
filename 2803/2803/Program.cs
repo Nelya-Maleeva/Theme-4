9f4d66ea1bbb7bd508272496587383f1e0dde156
Nelya-Maleeva <nelyamaleeva@gmail.com>
2023-03-28 12:16:36 +0300

    123 test 2803

---
+++ 2803/2803/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2803
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Nelya!");
+        }
+    }
+}
