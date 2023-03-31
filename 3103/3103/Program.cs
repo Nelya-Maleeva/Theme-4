502d55f354b5ff5298d84084b5bea9ddd931a2b1
Nelya-Maleeva <nelyamaleeva@gmail.com>
2023-03-31 16:10:39 +0300

    12334

--- 3103/3103/Program.cs
+++ 3103/3103/Program.cs
@@ -8,5 +8,13 @@ namespace _3103
         {
             Console.WriteLine("Hello World NELYA!");
         }
-    }
+        public void DisplayTest ()
+        {int a = 7;
+        int b = 4;
+        int c = 3;
+        int d = (a + b) / c;
+        int e = (a + b) % c;
+        Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");}
+}
 }
