dd00652d483b7bd3f5ac0993b99b7f434e98b04e
Nelya-Maleeva <nelyamaleeva@gmail.com>
2023-08-17 11:33:42 +0300

    411

---
+++ 411/411/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _411
+{
+    class CoffeeMachine
+    {
+
+    }
+    public class CoffeeMachinePhilips : CoffeeMachine
+    {
+
+    }
+    public abstract class CoffeeOptions
+    {
+        public abstract void MakeLatte();
+    }
+    public class Coffee : CoffeeOptions
+    {
+        public override void MakeLatte();
+    }
+    public class Barista
+    {
+        private void MakingSecretCoffee();
+        public virtual void MakingEspresso();
+    }
+    public class Trainee : Barista
+    {
+        public override void MakingEspresso();
+    }
+}
